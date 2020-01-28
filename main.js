6491adf6baf362f5da1662d711f224c227d8419e
Jean Correia <jeancorreia@dev-workstation.mylab.local>
2020-01-28 11:15:54 -0300

    Ajuste variavel versao build Jenkinsfile

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World! - v16');
+  response.end('Hello World! - v28/01-11:15');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
