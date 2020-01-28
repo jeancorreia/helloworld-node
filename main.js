a92c02eef31fed78bb3cf000683916c99224bec2
Jean Correia <jeancorreia@dev-workstation.mylab.local>
2020-01-28 15:03:49 -0300

    Teste 15:03

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World! - v28/01-11:15');
+  response.end('Hello World! - v28/01-15:03');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
